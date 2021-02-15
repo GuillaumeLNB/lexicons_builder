15b2fe55ef04d65c306e7e4392a23840f9a7d3cb
GLNB <guillaume.le-noe-bienvenu@irisa.fr>
2021-02-15 19:11:03 +0100

    added __init__ files

---
+++ src/graphs/__init__.py
@@ -0,0 +1,3 @@
+"""The Graph package contains the rdflib.Graph-ish
+class that all of the modules need to output the
+results"""
---
+++ src/wordnet_explorer/__init__.py
@@ -0,0 +1,17 @@
+"""This module contains the function that is used to get synonyms from
+`WORDNET <https://wordnet.princeton.edu/>`_ .
+
+Like the other modules, the exploration is done reccursively.
+
+"""
+
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = __name__
+    __version__ = get_distribution(dist_name).version
+except DistributionNotFound:
+    __version__ = "unknown"
+finally:
+    del get_distribution, DistributionNotFound
