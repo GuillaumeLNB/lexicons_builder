ce6fc24c15a37964af16d76969116bb55b26ab48
GLNB <guillaume.le-noe-bienvenu@irisa.fr>
2021-02-25 14:43:58 +0100

    added path

--- tests/download_nlp_models.sh
+++ tests/download_nlp_models.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-mkdir nlp_models 2>/dev/null ;
+mkdir /nfs/nas4/readit/readit/nlp_models 2>/dev/null ;
 i=0;
 for model in $(cat model_urls);
 do
     echo "MODEL $i OF $(wc -l model_urls)";
     let "i++"
-    wget $model -P nlp_models/;
+    wget $model -P /nfs/nas4/readit/readit/nlp_models/;
 
 done
 
 
-for file in nlp_models/*bz2;
+for file in /nfs/nas4/readit/readit/nlp_models/*bz2;
 do
     bzip2 -d $file;
 
 done
 
-for file in nlp_models/*zip;
+for file in /nfs/nas4/readit/readit/nlp_models/*zip;
 do
     unzip $file;
 
