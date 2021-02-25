937b5c1cbe114746fdc2030c6dfe5c6030759a83
GLNB <guillaume.le-noe-bienvenu@irisa.fr>
2021-02-25 10:13:45 +0100

    added __init__files

---
+++ lexicons_builder/__init__.py
@@ -0,0 +1,108 @@
+import logging
+
+from lexicons_builder.nlp_model_explorer.explorer import explore_nlp_model
+from lexicons_builder.scrappers.scrappers import get_synonyms_from_scrappers
+from lexicons_builder.wordnet_explorer.explorer import explore_wordnet, explore_wolf
+
+
+def build_lexicon(
+    words: list,
+    lang: str,
+    depth: int,
+    nlp_model_paths: list = [],
+    wolf_path: str = None,
+    wordnet: bool = False,
+    web: bool = True,
+):
+    """This is the main function to build lexicons.
+
+    Args:
+      words ([str]): List of words
+      lang (str): The language in ISO-639-1 (fr, en, de, ...)
+      depth (int): The depth to which it will go
+      nlp_model_paths ([str], optional): List of path to the nlp models
+      wolf_path (str, optional): The path to WOLF
+      wordnet (bool, optional): Retrieve related terms using WordNet
+      web (bool, optional): Retrieve related terms looking online
+
+    Returns:
+        :obj:`lexicons_builder.Graph`: a :py:meth:`lexicons_builder.Graph` object that contains the results.
+
+    .. code:: python
+
+        >>> from lexicons_builder import build_lexicon
+        >>> # search for words related to 'book' and 'read'
+        >>> # in WordNet and online
+        >>> g = build_lexicon(['book', 'read'], 'en', 1, wordnet=True, web=True)
+        >>> print(g)
+        @prefix ns1: <http://www.w3.org/2004/02/skos/core#> .
+        @prefix ns2: <urn:default:baseUri:#> .
+        @prefix ns3: <http://taxref.mnhn.fr/lod/property/> .
+        @prefix ns4: <http://www.w3.org/2006/03/wn/wn20/schema/> .
+        @prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+
+        ns2:note ns3:isSynonymOf ns2:root_word_uri ;
+            ns1:prefLabel "note" ;
+            ns2:comesFrom <synonyms.reverso.net> ;
+            ns2:depth 1 .
+
+        ns2:learned ns3:isSynonymOf ns2:root_word_uri ;
+            ns1:prefLabel "learned" ;
+            ns2:comesFrom <synonyms.com> ;
+            ns2:depth 1 .
+        ...
+        >>> g.to_list()
+        ['Bible', 'Book', 'Christian_Bible', 'Epistle', 'Good_Book', 'Holy_Scripture', 'Holy_Writ', 'Koran', 'PS', 'Quran', 'Scripture', 'Word', 'Word_of_God', 'accept', 'accommodate', 'according to the rules', 'account book', 'account_book', 'accountability', 'accountancy', 'accountant', 'accounting', 'accounts', 'accumulation', 'accuse', 'acquire', 'act', 'adjudge', 'admit', 'adopt', 'afl', 'aggregation', 'agree', 'aim', "al-Qur'an"
+        ...
+        >>> print(g.to_text_file())
+        book
+        read
+            Bible
+            Book
+            Christian_Bible
+            Epistle
+            Good_Book
+            Holy_Scripture
+            Holy_Writ
+            Koran
+            PS
+            Quran
+        ...
+
+    """
+
+    assert isinstance(words, list)
+    graphs = []
+    for word in words:
+        assert isinstance(word, str)
+        if nlp_model_paths:
+            for model in nlp_model_paths:
+                # looking for words in nlp models
+                logging.info(
+                    f"exploring model '{model}' with word '{word}' at {depth} depth"
+                )
+                graphs.append(explore_nlp_model(word, model, depth))
+        # looking for words online
+        if web:
+            logging.info(
+                f"looking up synonyms online for word '{word}' at depth {depth}"
+            )
+            graphs.append(get_synonyms_from_scrappers(word, lang, depth))
+        # looking for word with WOLF
+        if wolf_path:
+            logging.info(
+                f"exploring WOLF with word '{word}' at depth {depth} WOLF PATH IS '{wolf_path}'"
+            )
+            graphs.append(explore_wolf(word, wolf_path, depth))
+        # looking for word with WORDNET
+        if wordnet:
+            logging.info(f"exploring WORNET with word '{word}' at depth {depth}")
+            graphs.append(explore_wordnet(word, lang, depth))
+
+    # merging the graphs
+    main_graph = graphs[0]
+    for g in graphs[1:]:
+        main_graph += g
+    # setting the root words attributes
+    main_graph._set_root_word_attribute()
+    return main_graph
---
+++ lexicons_builder/__main__.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+"""
+Main script to retrive data from the web, wordnet and language models
+"""
+
+import argparse
+import logging
+import sys
+
+from touch import touch
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] -[%(name)s] - [%(levelname)s] - %(message)s",
+    handlers=[logging.StreamHandler()],
+)
+
+from lexicons_builder import build_lexicon
+
+
+def parse_args(arguments):
+    parser = argparse.ArgumentParser(
+        description="lexicon_builder, a tool to build lexicons"
+    )
+    parser.add_argument(
+        "words", nargs="+", help="The word(s) we want to get synonyms from"
+    )
+    parser.add_argument("-l", "--lang", help="The word language")
+    parser.add_argument(
+        "-m",
+        "--nlp-model",
+        dest="nlp_model_paths",
+        help="The path to the nlp model(s)",
+        nargs="+",
+    )
+    parser.add_argument(
+        "-wolf",
+        "--wolf-path",
+        dest="wolf_path",
+        help="The path to WOLF (French wordnet)",
+        type=str,
+    )
+    parser.add_argument(
+        "-d",
+        "--depth",
+        dest="depth",
+        type=int,
+        default=2,
+        help="The depth we want to dig in the models, websites, ...",
+    )
+    parser.add_argument(
+        "-f",
+        "--format",
+        dest="format",
+        choices=("ttl", "txt"),
+        default="txt",
+        help="The wanted output format (txt with indentation or ttl)",
+    )
+    parser.add_argument(
+        "-o",
+        "--out-file",
+        dest="out_file",
+        help="The file where the results will be stored",
+    )
+    parser.add_argument(
+        "-w",
+        "--wordnet",
+        dest="wordnet",
+        action="store_true",
+        help="search on wordnet using nltk",
+    )
+    parser.add_argument(
+        "--web", dest="web", help="Search on dictionnaries online", action="store_true",
+    )
+    args = parser.parse_args(arguments)
+    # print(args.__dict__)
+
+    if len(arguments) <= 1 or not args.words:
+        parser.print_help()
+        raise parser.error(f"no word provided or no enough arguments")
+
+    if not args.out_file:
+        raise parser.error(f"out file required !")
+
+    if not args.lang:
+        raise parser.error(f"language required !")
+
+    if (
+        not args.web
+        and not args.wordnet
+        and not args.wolf_path
+        and not args.nlp_model_paths
+    ):
+        raise parser.error(
+            f"Nowhere to took up for words. Perharps you wanted to add the --web option?"
+        )
+
+    return args
+
+
+def main(arguments):
+
+    args = parse_args(arguments)
+
+    touch(args.out_file)
+
+    main_graph = build_lexicon(
+        args.words,
+        args.lang,
+        args.depth,
+        nlp_model_paths=args.nlp_model_paths,
+        wolf_path=args.wolf_path,
+        wordnet=args.wordnet,
+        web=args.web,
+    )
+
+    if args.format == "txt":
+        main_graph.to_text_file(args.out_file)
+    elif args.format == "ttl":
+        with open(args.out_file, "w") as f:
+            print(main_graph, file=f)
+            logging.info(f"{args.out_file}")
+
+    logging.info(f"done. {len(main_graph)} unique synonyms found")
+
+
+if __name__ == "__main__":
+
+    sys.exit(main(sys.argv[1:]))
