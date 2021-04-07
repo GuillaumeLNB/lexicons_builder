fb1ca5e8fd4990e58486b5f92bc3b0bf2de4cd3c
GLNB <guillaume.le-noe-bienvenu@irisa.fr>
2021-04-07 18:41:40 +0200

    deleted

--- tests/unit/_test_nlp_models.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for model in $(ls /nfs/nas4/readit/readit/nlp_models/*);
-do
-    echo -n "$model\t" >> results.tsv;
-    python3.6 load_nlp_model.py $model 2>> results.tsv;
-    echo "" >> results.tsv;
-done
