f582aca61f9e8febc00716516d387af5a1795342
GLNB <guillaume.le-noe-bienvenu@irisa.fr>
2021-05-10 10:50:11 +0200

    updated setup.py w/ project documentation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst") as f:
 
 setup(
     name="lexicons_builder",
-    version="0.1.0",
+    version="0.1.2",
     packages=[
         "lexicons_builder",
         "lexicons_builder.graphs",
@@ -13,11 +13,15 @@ setup(
         "lexicons_builder.wordnet_explorer",
         "lexicons_builder.nlp_model_explorer",
     ],
-    url="",
+    url="https://lexicons-builder.readthedocs.io",
     license="MIT",
     author="GLNB",
     author_email="glnb@gmail.com",
     description="lexicons_builder, a tool to create lexicons",
     long_description=long_description,
     install_requires=["gensim", "languagecodes", "banal", "nltk", "rdflib"],
+    project_urls={
+        'Documentation': 'https://lexicons-builder.readthedocs.io',
+        'Source': 'https://github.com/GuillaumeLNB/lexicons_builder',
+    }
 )
